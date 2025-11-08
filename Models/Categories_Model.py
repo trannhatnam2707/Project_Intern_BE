dde36fcee3a59cd4ca1f740d3be9b2245bc8e8e2
trannhatnam2707 <tran.nam27072003@gmail.com>
2025-11-08 13:57:16 +0700

    lỗi login sau khi tạm ổn api product

--- Models/Categories_Model.py
+++ Models/Categories_Model.py
@@ -1,7 +1,7 @@
+# Models/Category_Model.py
 from sqlalchemy import Column, Integer, String
-from Database.Connection import Base
 from sqlalchemy.orm import relationship
-
+from Database.Connection import Base
 
 class Category(Base):
     __tablename__ = "Categories"
@@ -9,4 +9,4 @@ class Category(Base):
     CategoryID = Column(Integer, primary_key=True, index=True)
     CategoryName = Column(String(100), unique=True, nullable=False)
     
-    products = relationship("Products", back_populates="category")
+    products = relationship("Product", back_populates="category", cascade="all, delete-orphan")
--- Service/Auth_Service.py
+++ Service/Auth_Service.py
@@ -4,9 +4,9 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 #-----Hash mật khẩu-------#
 def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
+    return pwd_context.hash(password[:72])
 
 #-----Kiểm tra mât khẩu-------#
 def verify_password(plain_password: str, hashed_pw: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_pw)
+    return pwd_context.verify(plain_password[:72], hashed_pw)
 
